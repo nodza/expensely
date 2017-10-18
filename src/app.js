4001f4355b4fd641e9341e0df166042067ef5322
nodza <noel.hwande@gmail.com>
2017-10-17 20:40:20 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+    
+ReactDOM.render(<h2>Hello... Is it me for whom you are looking?</h2>, document.getElementById('app'));
