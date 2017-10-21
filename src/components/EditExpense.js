69f81bd4a2c0b46da3d3225086d832880f9364bd
nodza <noel.hwande@gmail.com>
2017-10-21 12:33:28 -0500

    Added Edit functionality. Moved remove button. Configured Redux devtools.

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { editExpense, removeExpense } from '../actions/expenses';
+import EditExpenseForm from './ExpenseForm';
 
 const EditExpense = (props) => {
   return (
-    <div>Edit expense with id {props.match.params.id}</div>
+    <div>
+      <EditExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push('/');
+        }} />
+      <br />
+      <button onClick={()  => {
+        props.dispatch(removeExpense({id: props.expense.id }));
+        props.history.push('/');
+      }}>Delete Expense</button>
+    </div>
+
   )
 }
 
-export default EditExpense;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpense);
