b3ce56197df0df0ee92a1cd60ceb2a83c8cbed53
nodza <noel.hwande@gmail.com>
2017-10-26 14:55:41 -0500

    Added test cases for ExpenseForm. Added mock for moment.js.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
