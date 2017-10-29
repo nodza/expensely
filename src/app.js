940f7924b4123a1c4e8ba7fd2d47555236029456
nodza <noel.hwande@gmail.com>
2017-10-28 19:47:25 -0500

    Added environment variables to run a production Webpack build

--- src/app.js
+++ src/app.js
@@ -12,21 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-// store.subscribe(() => {
-//   const state = store.getState();
-//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// });
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 13000, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 23700, createdAt: 3400 }));
-store.dispatch(addExpense({ description: 'Electric bill', amount: 6000, createdAt: 1200 }));
-store.dispatch(addExpense({ description: 'Childcare', amount: 700, createdAt: 48000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
