a972d8cddccbf67756a2ecdf993dd5bb938a3606
nodza <noel.hwande@gmail.com>
2017-10-29 09:46:22 -0500

    Added date and number formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-// onRemoveExpense = () => {
-//   console.log("Removal in process...");
-// };
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
     <h4>{description}</h4>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>{numeral(amount / 100).format('$0,0.00')} - {moment(createdAt).format('MMMM Do, YYYY')}</p>
   </div>
 );
 
