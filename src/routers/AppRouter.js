0ea22c8683fe7fba30ba4e1ca882e873cbe4c4a0
nodza <noel.hwande@gmail.com>
2017-11-04 15:13:48 -0500

    Added private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,20 +9,19 @@ import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
-import Header from '../components/Header';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
   <div>
-    <Header />
     <Switch>
       <Route exact path="/" component={Login} />
-      <Route path="/dashboard" component={ExpenseDashboard} />
-      <Route path="/create" component={AddExpense} />
-      <Route path="/edit/:id" component={EditExpense} />
-      <Route path="/help" component={HelpPage} />
+      <PrivateRoute path="/dashboard" component={ExpenseDashboard} />
+      <PrivateRoute path="/create" component={AddExpense} />
+      <PrivateRoute path="/edit/:id" component={EditExpense} />
+      <PrivateRoute path="/help" component={HelpPage} />
       <Route component={NotFoundPage} />
     </Switch>
     </div>
