b62041611d8845b87fa1b4f99741044613664e5c
nodza <noel.hwande@gmail.com>
2017-11-07 08:05:00 -0600

    Styled forms and lists

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,16 +5,25 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-  <div>
-    {
-      props.expenses.length === 0 ? (
-        <p>There are no expenses to display</p>
-      ) : (
-        props.expenses.map((expense) => {
-          return <ExpenseListItem key={expense.id} {...expense} />;
-        })
-      )
-    }
+  <div className="container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item__message">
+            <span>There are no expenses to display</span>
+          </div>
+        ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem key={expense.id} {...expense} />;
+            })
+          )
+      }
+    </div>
   </div>
 );
 
